6eedcc185ecd3c48176ffcb75d913bf2ccc4783f
jotta008 <josecirer@gmail.com>
2020-06-11 23:06:42 -0300

    master

--- noti.php
+++ noti.php
@@ -7,7 +7,7 @@
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
     
 
-    $data = MercadoPago\Payment::find_by_id('josecirer@gmail.com');
+    $data = MercadoPago\Payment::find_by_id('7066011131');
     var_dump($data);
 ?>
 
