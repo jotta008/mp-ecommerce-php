4beac04d26cbeceb00c8e2e438027f3618541151
jotta008 <josecirer@gmail.com>
2020-06-11 23:04:59 -0300

    master

--- noti.php
+++ noti.php
@@ -5,7 +5,7 @@
     require __DIR__ .  '/vendor/autoload.php';
 
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
-    MercadoPago\SDK::setIntegratorId('dev_24c65fb163bf11ea96500242ac130004');
+    
 
     $data = MercadoPago\Payment::find_by_id('josecirer@gmail.com');
     var_dump($data);
@@ -27,4 +27,4 @@
 }
 
 </pre>
-<p>Notificación 1</p>
+<p>Notificación 2</p>
