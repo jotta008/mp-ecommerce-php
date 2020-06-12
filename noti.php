3d97a16b5ef8b6b76deae67e1cfd9a8bafc4ae42
jotta008 <josecirer@gmail.com>
2020-06-11 22:57:43 -0300

    master

--- noti.php
+++ noti.php
@@ -4,8 +4,8 @@
     MercadoPago\SDK::setAccessToken('APP_USR-843006151276019-121819-96152aa43ff9060702cf329979e6ff68-8714891');
     MercadoPago\SDK::setIntegratorId('dev_24c65fb163bf11ea96500242ac130004');
 
-    $data = MercadoPago\Payment::find_by_id($_GET['id']);
-    print_r($data);
+    $data = MercadoPago\Payment::find_by_id('josecirer@gmail.com');
+    print_r($data->preference_id);
 ?>
 
 <pre>
@@ -15,7 +15,7 @@
     "data":{
         "id":"<?php echo 'josecirer@gmail.com'?>"
     },
-    "date_created": "<?php echo explode('.', $data->date_created)[0] ?>z",
+    "date_created": "<?php echo explode('.', $data->date_created)[0] ?>",
     "id":<?php echo $data->order->id    ?>,
     "live_mode": true,
     "type":"payment",
