661fbd27a47814abb4db4b22538f397c5fe1162c
jotta008 <josecirer@gmail.com>
2020-06-12 10:38:28 -0300

    master

--- detail.php
+++ detail.php
@@ -1,5 +1,7 @@
 <?php
-    require __DIR__ .  './vendor/autoload.php';
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+    require __DIR__ .  '/vendor/autoload.php';
 
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
     MercadoPago\SDK::setIntegratorId('dev_24c65fb163bf11ea96500242ac130004');
