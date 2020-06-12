d2348466f7946111c3190777d7ebe8457bf1fef7
jotta008 <josecirer@gmail.com>
2020-06-11 23:47:41 -0300

    master

--- noti.php
+++ noti.php
@@ -1,12 +1,6 @@
 <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
     require __DIR__ .  '/vendor/autoload.php';
-
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
-    
-
     $data = MercadoPago\Payment::find_by_id('7066011131');
 ?>
 
